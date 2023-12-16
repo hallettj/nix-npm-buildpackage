cd7baff665391c0872c89d12d1c18bfc3c8483bd
Jesse Hallett <jesse@sitr.us>
2023-12-15 17:21:58 -0800

    add outputs for aarch64-linux

--- flake.nix
+++ flake.nix
@@ -1,30 +1,47 @@
 {
   description = "Build npm and yarn packages";
 
-  outputs = { self, nixpkgs }: {
-    legacyPackages.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.callPackage ./default.nix {};
-    overlays.default = final: prev: {
-      inherit (final.callPackage ./. { }) mkNodeModules buildNpmPackage buildYarnPackage;
-    };
-    checks.x86_64-linux = let
-      nixpkgs' = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      npm6 = import ./tests/buildNpmPackage {
-        pkgs = nixpkgs';
-        npm-buildpackage = self.legacyPackages.x86_64-linux.override {
-          nodejs = nixpkgs'.nodejs-14_x;
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      eachSystem = systems: f:
+        let
+          op = attrs: system:
+            let
+              ret = f system;
+              op = attrs: key: attrs // { ${key} = (attrs.${key} or { }) // { ${system} = ret.${key}; }; };
+            in
+            builtins.foldl' op attrs (builtins.attrNames ret);
+        in
+        builtins.foldl' op { } systems;
+    in
+    eachSystem systems (system:
+      {
+        legacyPackages = nixpkgs.legacyPackages.${system}.callPackage ./default.nix { };
+        overlays.default = final: prev: {
+          inherit (final.callPackage ./. { }) mkNodeModules buildNpmPackage buildYarnPackage;
         };
-      };
-      npm8 = import ./tests/buildNpmPackage {
-        pkgs = nixpkgs';
-        npm-buildpackage = self.legacyPackages.x86_64-linux.override {
-          nodejs = nixpkgs'.nodejs-18_x;
-        };
-      };
-      yarn = import ./tests/buildYarnPackage {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        npm-buildpackage = self.legacyPackages.x86_64-linux;
-      };
-    };
-  };
+        checks =
+          let
+            nixpkgs' = nixpkgs.legacyPackages.${system};
+          in
+          {
+            npm6 = import ./tests/buildNpmPackage {
+              pkgs = nixpkgs';
+              npm-buildpackage = self.legacyPackages.${system}.override {
+                nodejs = nixpkgs'.nodejs-14_x;
+              };
+            };
+            npm8 = import ./tests/buildNpmPackage {
+              pkgs = nixpkgs';
+              npm-buildpackage = self.legacyPackages.${system}.override {
+                nodejs = nixpkgs'.nodejs-18_x;
+              };
+            };
+            yarn = import ./tests/buildYarnPackage {
+              pkgs = nixpkgs.legacyPackages.${system};
+              npm-buildpackage = self.legacyPackages.${system};
+            };
+          };
+      });
 }
